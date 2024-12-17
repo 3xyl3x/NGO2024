a960c00ed317b8dadd3ce7c7525634bae870399d
David Törnros <david@dxgruppen.se>
2024-12-17 16:15:34 +0100

    Textruta till lista

--- src/panels/EmployeesPanel.java
+++ src/panels/EmployeesPanel.java
@@ -5,7 +5,7 @@
 package panels;
 import database.DatabaseManager;
 import java.util.ArrayList;
-import java.util.HashMap; 
+import javax.swing.DefaultListModel; 
 import models.Employee;
 
 /**
@@ -15,27 +15,25 @@ import models.Employee;
 public class EmployeesPanel extends javax.swing.JPanel {
  private MainWindow window;
  private DatabaseManager dbm;
-    /**
-     * Creates new form EmployeesPanel
-     */
+ private DefaultListModel<Employee> listModel; 
+
     public EmployeesPanel(MainWindow window,DatabaseManager dbm) {
         this.window=window;
         this.dbm=dbm;
         initComponents();
+      
+        listModel = new DefaultListModel<>();
+        employeeList.setModel(listModel); 
         listEmployees();
     }
     
     private void listEmployees() {
                ArrayList<Employee> employees = dbm.getEmployees();
-
-            jTextArea1.setText(""); // Rensa tidigare text
-            if (employees != null) {
-                for (Employee employee : employees) {
-                    jTextArea1.append(employee.toString()+ "\n");
-                }
-            } else {
-                jTextArea1.setText("Inga anställda hittades.");
-            }
+               if (employees!=null) {
+                   for (Employee emp : employees) {
+                       listModel.addElement(emp);
+                   }
+               }
     }
 
     /**
@@ -47,16 +45,15 @@ public class EmployeesPanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jTextArea1 = new javax.swing.JTextArea();
         jLabel1 = new javax.swing.JLabel();
         goBackButton = new javax.swing.JButton();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        employeeList = new javax.swing.JList<>();
+        newEmployeeButton = new javax.swing.JButton();
+        editEmployeeButton = new javax.swing.JButton();
+        deleteEmployeeButton = new javax.swing.JButton();
 
         setPreferredSize(new java.awt.Dimension(600, 600));
-
-        jTextArea1.setColumns(20);
-        jTextArea1.setRows(5);
-        jScrollPane1.setViewportView(jTextArea1);
 
         jLabel1.setText("Lista över anställda");
 
@@ -67,21 +64,45 @@ public class EmployeesPanel extends javax.swing.JPanel {
             }
         });
 
+        employeeList.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
+        employeeList.addListSelectionListener(new javax.swing.event.ListSelectionListener() {
+            public void valueChanged(javax.swing.event.ListSelectionEvent evt) {
+                employeeListValueChanged(evt);
+            }
+        });
+        jScrollPane2.setViewportView(employeeList);
+
+        newEmployeeButton.setText("Ny anställd");
+        newEmployeeButton.setEnabled(false);
+        newEmployeeButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                newEmployeeButtonActionPerformed(evt);
+            }
+        });
+
+        editEmployeeButton.setText("Ändra anställd");
+        editEmployeeButton.setEnabled(false);
+
+        deleteEmployeeButton.setText("Radera anställd");
+        deleteEmployeeButton.setEnabled(false);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
+                .addGap(34, 34, 34)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(136, 136, 136)
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 115, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(34, 34, 34)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(goBackButton)
-                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 377, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addContainerGap(240, Short.MAX_VALUE))
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 242, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(28, 28, 28)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(deleteEmployeeButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(editEmployeeButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(newEmployeeButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 115, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(goBackButton))
+                .addContainerGap(184, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -90,9 +111,16 @@ public class EmployeesPanel extends javax.swing.JPanel {
                 .addComponent(goBackButton)
                 .addGap(18, 18, 18)
                 .addComponent(jLabel1)
-                .addGap(18, 18, 18)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 417, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(66, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 269, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(newEmployeeButton)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(editEmployeeButton)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(deleteEmployeeButton)))
+                .addContainerGap(245, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
@@ -100,11 +128,26 @@ public class EmployeesPanel extends javax.swing.JPanel {
            window.showPanel("StartPanel");
     }//GEN-LAST:event_goBackButtonActionPerformed
 
+    private void newEmployeeButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_newEmployeeButtonActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_newEmployeeButtonActionPerformed
+
+    // En anställd vald 
+    private void employeeListValueChanged(javax.swing.event.ListSelectionEvent evt) {//GEN-FIRST:event_employeeListValueChanged
+       
+       boolean isSelected = !employeeList.isSelectionEmpty();
+       editEmployeeButton.setEnabled(isSelected);
+       deleteEmployeeButton.setEnabled(isSelected);
+    }//GEN-LAST:event_employeeListValueChanged
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton deleteEmployeeButton;
+    private javax.swing.JButton editEmployeeButton;
+    private javax.swing.JList<Employee> employeeList;
     private javax.swing.JButton goBackButton;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JButton newEmployeeButton;
     // End of variables declaration//GEN-END:variables
 }
