6707649a8cc0be345bccc81bbb180ad0d1bd5d30
Leia Ry Wetterstrand Bark <leiawetterstrand@hotmail.com>
2024-12-23 11:36:40 +0100

    lade till e-post syntax kontroll i validator
    kolla gärna igenom, ej helt säker på denna.

--- src/util/Validator.java
+++ src/util/Validator.java
@@ -1,5 +1,6 @@
 package util;
 import javax.swing.JTextField;
+import java.util.regex.Pattern;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -15,4 +16,17 @@ public class Validator {
     public static boolean textFieldHasValue(JTextField textField) {
        return (textField.getText().isEmpty()==false);
     }
+    
+       public static void main(String[] args) {
+        String email = "example@mail.com";
+        String emailRegex = "^[\\w-\\.]+@[\\w-]+\\.[a-zA-Z]{2,}$";
+
+        boolean isValid = Pattern.matches(emailRegex, email);
+
+        if (isValid) {
+            System.out.println("Valid mail");
+        } else {
+            System.out.println("Invalid mail");
+        }
+       }
 }
