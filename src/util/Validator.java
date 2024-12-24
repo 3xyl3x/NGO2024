13f43df3819f7835f574ba62c4cda70ca79dd59a
Leia Ry Wetterstrand Bark <leiawetterstrand@hotmail.com>
2024-12-25 00:09:09 +0100

    lade till kontroll av skadlig text
    osäker på .contains

--- src/util/Validator.java
+++ src/util/Validator.java
@@ -12,10 +12,22 @@ public class Validator {
        return (textField.getText().isEmpty()==false);
     }
 
-       // alternativ med endast booelan
-    public static boolean isValidEmail(JTextField textField) {
+       // kontroll av korrekt textformat av mail
+    public static boolean textFieldIsEmail(JTextField textField) {
         String emailRegex = "^[\\w-\\.]+@[\\w-]+\\.[a-zA-Z]{2,}$";
         return Pattern.matches(emailRegex, textField.getText());
     }
     
+    // kontroll av icke skadlig kod
+    public static boolean textFieldIsClean (JTextField textField) {
+        String input = textField.getText();
+        String forbiddeninput = "'|\"|;|--|\\b(SELECT|INSERT|DELETE|DROP|UPDATE|ALTER|CREATE|WHERE)\"\b,";
+       
+        if(input.contains(forbiddeninput)) {
+        return false;
+    }
+        else{
+    return true;
+        }
+    }
 }
