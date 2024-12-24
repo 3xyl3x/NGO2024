52ab8eb0c5bd931e672f049b49fa9b9fc2f90f40
David Törnros <126451299+3xyl3x@users.noreply.github.com>
2024-12-24 11:12:53 +0100

    Update Goal.java
    inga listor i modellerna, hanteras i panelern

--- src/models/Goal.java
+++ src/models/Goal.java
@@ -1,15 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package models;
-
-/**
- *
- * @author leia
- */
-import java.util.ArrayList;
-
 
 public class Goal {
     private int id;
@@ -17,31 +7,17 @@ public class Goal {
     private int number;
     private String description;
     private int priority;
-     private static ArrayList<Goal> goalsList = new ArrayList<>();
-   
+
 
 public Goal(int id, String name, int number, String description, int priority)
 {
-this.id = id;
-this.name = name;
-this.number = number;
-this.description = description;
-this.priority = priority;
-goalsList.add(this);
+    this.id = id;
+    this.name = name;
+    this.number = number;
+    this.description = description;
+    this.priority = priority;
 }
 
-public void showAllInfo()
-{
-    System.out.println("List of all goals:");
-    System.out.println( ); // blankrad
-        for (Goal goal : goalsList) {
-System.out.println("Name: " + name);
-System.out.println("Description: " + description);
-System.out.println("Number: " + number);
-System.out.println("Priority: " + priority);
-System.out.println( ); // blankrad
-}
-}
 
 public int getId()
 {
