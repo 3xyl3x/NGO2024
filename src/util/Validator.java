1bd9a9d38c287151bf3759fa64feceed5fd6562d
David Törnros <126451299+3xyl3x@users.noreply.github.com>
2024-12-24 11:11:37 +0100

    Update Validator.java
    Saker låg i konstruktorn som ej skulle det. Parameter skall vara textfält.

--- src/util/Validator.java
+++ src/util/Validator.java
@@ -2,39 +2,22 @@ package util;
 import javax.swing.JTextField;
 import java.util.regex.Pattern;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author david
- */
 public class Validator {
     
+
+    //kontroll med utskrift
+       public static void main(String[] args) {
+
+       }
+
     public static boolean textFieldHasValue(JTextField textField) {
        return (textField.getText().isEmpty()==false);
     }
-    
-    //kontroll med utskrift
-       public static void main(String[] args) {
-        String email = "example@mail.com";
-        String emailRegex = "^[\\w-\\.]+@[\\w-]+\\.[a-zA-Z]{2,}$";
-
-        boolean isValid = Pattern.matches(emailRegex, email);
-
-        if (isValid) {
-            System.out.println("Valid mail");
-        } else {
-            System.out.println("Invalid mail");
-        }
-       }
 
        // alternativ med endast booelan
-    public static boolean isValidEmail(String email) {
+    public static boolean isValidEmail(JTextField textField) {
         String emailRegex = "^[\\w-\\.]+@[\\w-]+\\.[a-zA-Z]{2,}$";
-        return Pattern.matches(emailRegex, email);
+        return Pattern.matches(emailRegex, textField.getText());
     }
     
 }
